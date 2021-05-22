50a041ccf287554a9a9774eed378d0947ca2b401
olgarkovich <olgagarkovich@gmail.com>
2021-05-22 15:20:03 +0300

    init: complete randomizer task

--- app/src/main/java/com/rsschool/android2021/FirstFragment.kt
+++ app/src/main/java/com/rsschool/android2021/FirstFragment.kt
@@ -1,17 +1,24 @@
 package com.rsschool.android2021
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 
 class FirstFragment : Fragment() {
 
     private var generateButton: Button? = null
     private var previousResult: TextView? = null
+    private lateinit var minValue: EditText
+    private lateinit var maxValue: EditText
+
+    private lateinit var fragmentSendRangeDataListener: OnFragmentSendRangeData
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,15 +32,18 @@ class FirstFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         previousResult = view.findViewById(R.id.previous_result)
         generateButton = view.findViewById(R.id.generate)
+        minValue = view.findViewById(R.id.min_value)
+        maxValue = view.findViewById(R.id.max_value)
 
         val result = arguments?.getInt(PREVIOUS_RESULT_KEY)
         previousResult?.text = "Previous result: ${result.toString()}"
 
-        // TODO: val min = ...
-        // TODO: val max = ...
-
         generateButton?.setOnClickListener {
-            // TODO: send min and max to the SecondFragment
+            val min = minValue.text.toString()
+            val max = maxValue.text.toString()
+            if (validateData(min, max)) {
+                fragmentSendRangeDataListener.sendRangeData(min.toInt(), max.toInt())
+            }
         }
     }
 
@@ -49,5 +59,38 @@ class FirstFragment : Fragment() {
         }
 
         private const val PREVIOUS_RESULT_KEY = "PREVIOUS_RESULT"
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        fragmentSendRangeDataListener = context as OnFragmentSendRangeData
+    }
+
+    interface OnFragmentSendRangeData {
+        fun sendRangeData(min: Int, max: Int)
+    }
+
+    private fun validateData(min: String, max: String): Boolean {
+        return if (min == "" || max == "") {
+            Toast.makeText(context, "Please, input values", Toast.LENGTH_SHORT).show()
+            false
+        } else if (min[0] == '-' || max[0] == '-') {
+            Toast.makeText(context, "Error, values should be positive or '0'", Toast.LENGTH_LONG)
+                .show()
+            false
+        } else if (min.toIntOrNull() == null || max.toIntOrNull() == null) {
+            Toast.makeText(
+                context,
+                "Error, values should be in range [0..2147483647]",
+                Toast.LENGTH_LONG
+            ).show()
+            false
+        } else if (min.toInt() > max.toInt()) {
+            Toast.makeText(context, "Error, min should not be bigger than max", Toast.LENGTH_LONG)
+                .show()
+            false
+        } else {
+            true
+        }
     }
 }
--- app/src/main/java/com/rsschool/android2021/SecondFragment.kt
+++ app/src/main/java/com/rsschool/android2021/SecondFragment.kt
@@ -1,17 +1,26 @@
 package com.rsschool.android2021
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import kotlin.random.Random
+import kotlin.random.nextInt
+
 
 class SecondFragment : Fragment() {
 
     private var backButton: Button? = null
     private var result: TextView? = null
+    private var randomNumber = 0
+
+    private lateinit var fragmentSendRandomNumberListener: OnFragmentSendRandomNumber
+    private lateinit var callback: OnBackPressedCallback
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,16 +38,18 @@ class SecondFragment : Fragment() {
         val min = arguments?.getInt(MIN_VALUE_KEY) ?: 0
         val max = arguments?.getInt(MAX_VALUE_KEY) ?: 0
 
-        result?.text = generate(min, max).toString()
+        randomNumber = generate(min, max)
+        result?.text = randomNumber.toString()
 
         backButton?.setOnClickListener {
-            // TODO: implement back
+            fragmentSendRandomNumberListener.sendRandomNumber(randomNumber)
         }
+
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
     }
 
     private fun generate(min: Int, max: Int): Int {
-        // TODO: generate random number
-        return 0
+        return Random.nextInt(min..max)
     }
 
     companion object {
@@ -47,13 +58,28 @@ class SecondFragment : Fragment() {
         fun newInstance(min: Int, max: Int): SecondFragment {
             val fragment = SecondFragment()
             val args = Bundle()
-
-            // TODO: implement adding arguments
-
+            args.putInt(MIN_VALUE_KEY, min)
+            args.putInt(MAX_VALUE_KEY, max)
+            fragment.arguments = args
             return fragment
         }
 
         private const val MIN_VALUE_KEY = "MIN_VALUE"
         private const val MAX_VALUE_KEY = "MAX_VALUE"
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        fragmentSendRandomNumberListener = context as OnFragmentSendRandomNumber
+
+        callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                fragmentSendRandomNumberListener.sendRandomNumber(randomNumber)
+            }
+        }
+    }
+
+    interface OnFragmentSendRandomNumber {
+        fun sendRandomNumber(randomNumber: Int)
     }
 }
